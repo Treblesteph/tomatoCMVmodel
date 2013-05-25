33b3bac0a6c7ea8c70ee63f77e77e4803db53972
Stephanie Unna <steph.unna@gmail.com>
2013-05-25 18:26:07 +0100

    allow repeats, plot population size and proportion over time

--- analysis/plot.R
+++ analysis/plot.R
@@ -1,43 +1,47 @@
-  # load data
-  sv <- read.csv('../season_visits.csv')
-  hundredseasons <- read.csv('../1000_seasons.csv')
-  
-  # plot hour visits
-  library(ggplot2)
-  library(reshape)
-  sv_melted <- melt(sv, id="hour")
-  ggplot(data=sv_melted,
-         aes(x=hour, y=value, colour=variable)) +
-    # geom_line() + # uncomment line to plot unsmoothed data
-    stat_smooth(method='loess') +
-    labs(colour = "Population") +
-    ylab("population size")
-  
-  # plot just susceptible vs resistant
-  sv_sr <- data.frame(hour=sv$hour, S=rowSums(sv[,c(3,4)]), R=rowSums(sv[,c(2,5,6)]))
-  sv_sr_melted <- melt(sv_sr, id="hour")
-  ggplot(data=sv_sr_melted,
-         aes(x=hour, y=value, colour=variable)) +
-    # geom_line() + # uncomment line to plot unsmoothed data
-    stat_smooth(method='loess') +
-    labs(colour = "Population") +
-    ylab("population size")
-  
-  # plot 100season population count
-  hs_melted <- melt(hundredseasons, id="season")
-  ggplot(data=hs_melted,
-         aes(x=season, y=value, colour=variable)) +
-    # geom_line() + # uncomment line to plot unsmoothed data
-    stat_smooth(method='loess') +
-    labs(colour = "Population") +
-    ylab("population size")
-  
-  # plot just susceptible vs resistant for 110season
-  hs_sr <- data.frame(season=hundredseasons$season, S=rowSums(hundredseasons[,c(3,4)]), R=rowSums(hundredseasons[,c(2,5,6)]))
-  hs_sr_melted <- melt(hs_sr, id="season")
-  ggplot(data=hs_sr_melted,
-         aes(x=season, y=value, colour=variable)) +
-    # geom_line() + # uncomment line to plot unsmoothed data
-    stat_smooth(method='loess') +
-    labs(colour = "Population") +
-    ylab("population size")
+# load data
+library(ggplot2)
+library(reshape)
+sv <- read.csv('../season_visits.csv')
+thousandseasons <- read.csv('../1000_seasons.csv')
+hundredseasons <- thousandseasons[thousandseasons$season <= 100,]
+fortyseasons <- thousandseasons[thousandseasons$season <= 40,]
+
+linePlot <- function(df, size) {
+  # plot susceptible vs resistant for 1000 seasons
+  pd <- position_dodge(0)  # in case error bars or points overlap, they can be dodged by setting to 0.1
+  ggplot(data=df,
+         aes(x=season, y=mean, colour=population)) +
+    geom_errorbar(aes(ymin=mean-sem, ymax=mean+sem), width=.1, position=pd) +
+    geom_line(position=pd) +
+    # geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
+    scale_colour_hue(name="Population", # Legend label, use darker colors
+                     l=40) + # Use darker colors, lightness=40
+    ylab("Population size") +
+    xlab("Season") +
+    ggtitle(paste("Size of resistant and susceptible populations over", size, "seasons")) +
+    theme_bw() +
+    theme(legend.justification=c(1,0), legend.position=c(0.95,0.5)) # Position legend in mid right
+}
+
+stackedBarPlot <- function(df, size) {
+  # same but as porportional stacked bars
+  ggplot(data=df,
+         aes(x=season, y=mean, fill=population)) +
+    geom_bar(position="fill", stat="identity", width=0.99) +
+    ylab("Proportion of popoulation") +
+    xlab("Season") +
+    scale_fill_hue(name="Population", # Legend label, use darker colors
+                     l=30) +
+    ggtitle(paste("Proportion of resistant and susceptible populations over", size, "seasons")) +
+    theme_bw() +
+    theme(legend.justification=c(1,0), legend.position=c(0.95,0.5)) # Position legend in mid right
+}
+
+pdf("testplots.pdf")
+linePlot(thousandseasons, "1000")
+stackedBarPlot(thousandseasons, "1000")
+linePlot(hundredseasons, "100")
+stackedBarPlot(hundredseasons, "100")
+linePlot(fortyseasons, "40")
+stackedBarPlot(fortyseasons, "40")
+dev.off()
